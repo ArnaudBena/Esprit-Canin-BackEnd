b7d410f28e09f59e77a3690021b4aa4281e8d091
Arnaud BENACQUISTA <arnaud.benacquista@gmail.com>
2026-03-27 10:47:28 +0100

    Ãfeat: finish RaceController

--- src/main/java/edu/mns/cda/clubcaninbackend/controller/RaceController.java
+++ src/main/java/edu/mns/cda/clubcaninbackend/controller/RaceController.java
@@ -3,11 +3,20 @@ package edu.mns.cda.clubcaninbackend.controller;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import edu.mns.cda.clubcaninbackend.dao.RaceDao;
+import edu.mns.cda.clubcaninbackend.model.Race;
+import edu.mns.cda.clubcaninbackend.view.RaceView;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Tag(name = "Race", description = "API pour manipuler les races")
@@ -19,5 +28,83 @@ public class RaceController {
 
 
     @GetMapping("/list")
-    @JsonView("")
+    @JsonView(RaceView.class)
+    public List<Race> getAllRaces() {
+        return raceDao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @JsonView(RaceView.class)
+    @Operation(
+            summary = "Fetch race par id",
+            description = "Fetch de la race par l'id dans l'URL'"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Operation Reussie"),
+            @ApiResponse(responseCode = "404", description = "Cet ID de race n'existe pas")
+    })
+    public ResponseEntity<Race> get(@PathVariable int id) {
+
+        Optional<Race> optionalRace = raceDao.findById(id);
+
+        if(optionalRace.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(optionalRace.get(), HttpStatus.OK);
+
+    }
+
+    @PostMapping()
+    @JsonView(RaceView.class)
+    public ResponseEntity<Race> createRace(
+            @RequestBody
+            @Valid() // vérif à effectuer ici
+            Race raceToInsert) {
+
+        raceToInsert.setId(null);
+
+        raceDao.save(raceToInsert);
+
+        return new ResponseEntity<>(raceToInsert, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Race> deleteRace(@PathVariable int id) {
+
+        Optional<Race> optionalRace = raceDao.findById(id);
+
+        if(optionalRace.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        raceDao.deleteById(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateRace(
+            @PathVariable int id,
+            @RequestBody
+            @Valid
+            Race raceToUpdate) {
+
+        Optional<Race> optionalRace = raceDao.findById(id);
+
+        if(optionalRace.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        // On écrase l'id du json par celui en paramètre
+        raceToUpdate.setId(id);
+
+        // On réaffecte les anciennes valeurs qui ne doivent pas être changé
+        raceToUpdate.setNom(raceToUpdate.getNom());
+
+        raceDao.save(raceToUpdate);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
