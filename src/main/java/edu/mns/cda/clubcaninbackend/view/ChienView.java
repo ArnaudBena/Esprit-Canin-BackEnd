363412b2da70c37c66b781294be9dfa89ac66e35
ArnaudBena <arnaud.benacquista@gmail.com>
2026-04-13 11:34:19 +0200

    feat: add chienView

--- src/main/java/edu/mns/cda/clubcaninbackend/view/ChienView.java
+++ src/main/java/edu/mns/cda/clubcaninbackend/view/ChienView.java
@@ -1,3 +1,6 @@
 package edu.mns.cda.clubcaninbackend.view;
 
+import edu.mns.cda.clubcaninbackend.model.Chien;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface ChienView {}
