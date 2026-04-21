307180349d1df41eb7ed9941b85317315dd70910
ArnaudBena <arnaud.benacquista@gmail.com>
2026-04-21 09:40:21 +0200

    refactor : enrichissement du ddata-init.sql

--- src/main/resources/data-init.sql
+++ src/main/resources/data-init.sql
@@ -1,4 +1,18 @@
+-- Races
 INSERT INTO race (nom) VALUES
-     ('Labrador'),
-     ('Berger Malinois'),
-     ('Caniche');
+    ('Labrador'),
+    ('Berger Malinois'),
+    ('Caniche');
+
+-- Roles
+INSERT INTO role (nom, description) VALUES
+    ('ADHERENT', 'Utilisateur inscrit au club, peut inscrire ses chiens aux séances'),
+    ('COACH', 'Éducateur canin habilité à animer les séances'),
+    ('ADMIN', 'Administrateur du club, gère les utilisateurs et le catalogue');
+
+-- Compétences
+INSERT INTO competence (nom, description) VALUES
+    ('Assis', 'Le chien s''assoit sur commande et maintient la position'),
+    ('Rappel', 'Le chien revient immédiatement vers son maître à l''appel'),
+    ('Marche en laisse', 'Le chien marche calmement en laisse sans tirer'),
+    ('Pas bouger', 'Le chien reste immobile malgré les distractions');
