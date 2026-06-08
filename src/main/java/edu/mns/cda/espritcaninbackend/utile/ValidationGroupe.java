c62131836e2e14c4ee347d2b106426a2f714583f
ArnaudBena <arnaud.benacquista@gmail.com>
2026-06-08 11:54:00 +0200

    feat(auth): groupe de validation OnInscription (role non requis a l'inscription)

--- src/main/java/edu/mns/cda/espritcaninbackend/utile/ValidationGroupe.java
+++ src/main/java/edu/mns/cda/espritcaninbackend/utile/ValidationGroupe.java
@@ -4,4 +4,5 @@ public interface ValidationGroupe {
     public interface OnUpdate{};
     public interface OnCreate{};
     public interface OnProfilUpdate{}; // self-service adhérent : valide nom/prénom/email/téléphone, PAS le rôle
+    public interface OnInscription{};  // inscription publique : nom/prénom/email/password/téléphone, PAS le rôle (forcé Adherent côté serveur)
 }
