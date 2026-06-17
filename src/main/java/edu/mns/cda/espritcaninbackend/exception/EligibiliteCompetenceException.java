e9e735159c29b7e6506f8037ddb5aa1f647989b5
ArnaudBena <arnaud.benacquista@gmail.com>
2026-06-17 21:30:24 +0200

    fix(inscription): fix sur inscription pour la règle de necessité de compétence

--- src/main/java/edu/mns/cda/espritcaninbackend/exception/EligibiliteCompetenceException.java
+++ src/main/java/edu/mns/cda/espritcaninbackend/exception/EligibiliteCompetenceException.java
@@ -5,6 +5,6 @@ import edu.mns.cda.espritcaninbackend.model.NiveauCompetence;
 public final class EligibiliteCompetenceException extends RuntimeException {
     public EligibiliteCompetenceException(String competenceNom, NiveauCompetence requis, NiveauCompetence actuel) {
         super("Le chien n'a pas le niveau requis en '" + competenceNom + "' "
-                + "(requis : " + requis + ", actuel : " + actuel + ")");
+                + "(requis : " + requis + ", actuel : " + (actuel == null ? "non acquis" : actuel) + ")");
     }
 }
